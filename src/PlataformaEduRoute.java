72a213fc71e33ef05d000c6e61a96d8391b22a4e
Claudia <claudia.robles@udla.edu.ec>
2025-11-22 21:59:26 -0500

    segundo commit

--- src/PlataformaEduRoute.java
+++ src/PlataformaEduRoute.java
@@ -1,5 +1,24 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PlataformaEduRoute {
     private JPanel Ventana;
+    private JTabbedPane tabbedPane1;
+    private JTextField txtIdEstudiante;
+    private JTextField txtNombreEstudiante;
+    private JTextField txtCurso;
+    private JComboBox cbDireccion;
+    private JComboBox cbPrioridad;
+    private JButton btnIngresarEst;
+    private JComboBox cbParada;
+
+    public PlataformaEduRoute() {
+        btnIngresarEst.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
